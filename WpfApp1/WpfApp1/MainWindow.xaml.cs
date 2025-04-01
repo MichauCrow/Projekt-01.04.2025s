9d47b505603921503fed2d5a6d765e8336a48dc5
MichauCrow <113516509+MichauCrow@users.noreply.github.com>
2025-04-01 15:21:28 +0200

    Add files via upload

---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Sname { get; set; }
+        public string PESEL { get; set; }
+        
+    }
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            Person item = new Person();
+            item.Name = "Michal";
+            item.Sname = "Gawron";
+            item.PESEL = "12432543623";
+            var rnd = new Random();
+
+            listview.Items.Add(new { m_nID = rnd.Next(), m_strName = item.Name });
+            
+        }
+    }
+}
